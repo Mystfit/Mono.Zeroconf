eb94a19e136efc1ae38456db1ebffce2438aefff
Byron Mallett <byronated@gmail.com>
2015-11-05 01:52:26 +1300

    Applied port byte endian patch fix

--- src/Mono.Zeroconf.Providers.Bonjour/Mono.Zeroconf.Providers.Bonjour/Service.cs
+++ src/Mono.Zeroconf.Providers.Bonjour/Mono.Zeroconf.Providers.Bonjour/Service.cs
@@ -132,9 +132,21 @@ namespace Mono.Zeroconf.Providers.Bonjour
             set { UPort = (ushort)value; }
         }
 
-        public ushort UPort {
-            get { return port; }
-            set { port = value; }
+        public ushort UPort
+        {
+            get
+            {
+                if (BitConverter.IsLittleEndian)
+                    return (ushort)(((port & 0xFF) << 8) | ((port & 0xFF00) >> 8));
+                return port;
+            }
+            set
+            {
+                if (BitConverter.IsLittleEndian)
+                    port = (ushort)(((port & 0xFF) << 8) | ((port & 0xFF00) >> 8));
+                else
+                    port = value;
+            }
         }
     }
 }
